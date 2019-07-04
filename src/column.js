7b5e3a719e6a0da94b9ab58f074229d714e0b7b9
Peter Mercer <Peter.Mercer@nps.org.au>
2019-07-04 17:32:41 +1000

    halfway through tutorial

--- src/column.js
+++ src/column.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
 import Task from './task';
+import {Droppable} from "react-beautiful-dnd";
 
 const Container = styled.div`
 margin: 8px;
 border: 1px solid lightgrey;
 border-radius: 2px;
 `;
-const Title = styled.h3``;
-const TaskList = styled.div``;
+const Title = styled.h3`
+padding: 8px;`;
+const TaskList = styled.div`
+padding:8px;
+transition: background-color 0.5s ease;
+background-color: ${props => (props.isDraggingOver? 'lightblue': 'white')}
+`;
 
 export default props => (
     <Container>
-        <Title>{props.title}</Title>
-        <TaskList>{props.tasks.map((task) => <Task key={task.id} task={task} />)}</TaskList>
+        <Title>{props.column.title}</Title>
+        <Droppable droppableId={props.column.id}>
+
+        { (provided, snapshot) => (
+            <TaskList
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+                isDraggingOver={snapshot.isDraggingOver}
+            >{props.tasks.map((task, index) => <Task key={task.id} task={task} index={index} />)}
+                {provided.placeholder}
+            </TaskList>
+         )
+        }
+
+        </Droppable>
     </Container>
 )
--- src/index.js
+++ src/index.js
@@ -1,20 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import '@atlaskit/css-reset';
 import initialData from './initial-data';
 import Column from './column';
+import {DragDropContext} from "react-beautiful-dnd";
 
 
 const App = () => {
-    const state = initialData;
+
+    const [dragState, setDragState] = useState(initialData);
+
+    const _onDragEnd = result => {
+        // Add later
+        const {destination, source, draggableId} = result;
+        if(!destination){
+            return;
+        }
+
+        if(destination.droppableId === source.droppableId &&
+            destination.index === source.index){
+            return;
+        }
+
+        const column = dragState.columns[source.droppableId];
+        const newTaskIds = Array.from(column.taskIds);
+        newTaskIds.splice(source.index, 1);
+        newTaskIds.splice(destination.index, 0, draggableId);
+
+        const newColumn = {
+            ...column,
+            taskIds: newTaskIds
+        };
+
+        const newState = {
+            ...dragState,
+            columns: {
+                ...dragState.columns,
+                [newColumn.id]: newColumn
+            }
+        }
+
+        setDragState(newState);
+    }
 
     return (
-        state.columnOrder.map((columnId) => {
-            const column = state.columns[columnId];
-            const tasks = column.taskIds.map(taskId => state.tasks[taskId]);
+        <DragDropContext
+            onDragEnd={_onDragEnd}
+
+        >
+        {dragState.columnOrder.map((columnId) => {
+            const column = dragState.columns[columnId];
+            const tasks = column.taskIds.map(taskId => dragState.tasks[taskId]);
             return <Column key={column.id} column={column} tasks={tasks} />;
 
-        })
+        })}
+        </DragDropContext>
     )
 }
 
